e026c4fc13bbb7cc0b507c9bf4e3eb7f56ad331f
Asaf Yacobovich <asafyaco@gmail.com>
2022-07-05 10:41:48 +0300

    Merged 2 fetches to one with an if statement on main

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,7 +1,5 @@
-import { Box } from "@mui/system";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import DataGridComponent from "../../components/DataGrid/DataGrid";
-import Pagination from "@mui/material/Pagination";
 import SearchAndFilters from "../SearchAndFilters/SearchAndFilters";
 
 const Main = () => {
@@ -9,58 +7,52 @@ const Main = () => {
     const [pageSize, setPageSize] = useState(20);
     const [countPages, setCountPages] = useState();
     const [pageNumber, setPageNumber] = useState(1);
-    const [filteredResults, setFilteredResults] = useState(null);
+    const [data, setData] = useState(null);
     const [showPagination, setShowPagination] = useState(true);
   
     const [search, setSearch] = useState("");
     const [filterGender, setFilterGender] = useState("");
     const [filterStatus, setFilterStatus] = useState("");
 
+    const [gender, setGender] = useState("");
+    const [status, setStatus] = useState("");
+
+    const [doEffect, setDoEffect] = useState(true);
+
     // popup
     const [open, setOpen] = useState(false);
     const [popupContent, setPopupContent] = useState(true);
+    
+    const [filterTriggered, setFilterTriggered] = useState(false);
 
     const handleOpen = (row) => {setOpen(true); setPopupContent(row)};
+    
     const handleClose = () => setOpen(false);
 
     const filterCharacters = (pageNumber, gender, status, search) => {
-      let temp = [];
-      let url = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search ? search : ''}`;
-      url += gender ? `&gender=${gender}` : '';
-      url += status ? `&status=${status}` : '';
-      fetch(url)
-        .then((resultData) => {
-          return resultData.json();
-        })
-        .then((data) => {
-          setCountPages(data.info.pages);
-          // console.log(data?.results);
-            temp = data?.results.map((character) => {
-              return {
-                id : character.id,
-                col1 : character.image,
-                col2 : character.name,
-                col3 : character.origin?.name,
-                col4 : character.status,
-                col5 : character.species,
-                col6 : character.gender,
-                episodes: character.episode
-              };
-            });
-            setFilteredResults(temp);
-            setShowPagination(true);
-          })
-        .catch(() => {
-          setCountPages(1);
-          setFilteredResults([]);
-          setShowPagination(false);
-        });
+      setSearch(search);
+      setGender(gender);
+      setStatus(status);
+      setPageNumber(pageNumber);
+      setFilterTriggered(true);
+      setDoEffect(!doEffect);
     };
   
     useEffect(() => {
+      console.log("Starting fetch... ");
       let arr = [];
-      let initURL = `https://rickandmortyapi.com/api/character?page=${pageNumber}`;
-      fetch(initURL)
+      let url = "";
+      if(filterTriggered == true) {
+        url = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${
+          search ? search : ""
+      }`;
+      url += gender ? `&gender=${gender}` : "";
+      url += status ? `&status=${status}` : "";
+      } else {
+        url = `https://rickandmortyapi.com/api/character?page=${pageNumber}`;
+      }
+      
+      fetch(url)
         .then((res) => res.json())
         .then((json) => {
           json && setCountPages(json.info.pages);
@@ -78,16 +70,22 @@ const Main = () => {
           });
         })
         .then(() => {
-          // console.log(arr);
-          setFilteredResults(arr);
+          setData(arr);
           setCharacters(arr);
+          setShowPagination(true);
+        })
+        // If there is no data that fits to the search
+        .catch(() => {
+          setCountPages(1);
+          setData([]);
+          setShowPagination(false);
         });
-    }, []);  
+  }, [doEffect]);
 
   return (
     <div className="rnm-page">
       <SearchAndFilters search={search} pageNumber={pageNumber} filterGender={filterGender} filterStatus={filterStatus} setPageNumber={setPageNumber} setSearch={setSearch} filterCharacters={filterCharacters} setFilterGender={setFilterGender} setFilterStatus={setFilterStatus}/>
-          <DataGridComponent filteredResults={filteredResults} pageSize={pageSize} setPageSize={setPageSize} setPageNumber={setPageNumber} filterCharacters={filterCharacters} filterGender={filterGender} filterStatus={filterStatus} search={search} countPages={countPages} pageNumber={pageNumber} showPagination={showPagination} open={open} handleOpen={handleOpen} popupContent={popupContent} handleClose={handleClose}/>
+          <DataGridComponent filteredResults={data} pageSize={pageSize} setPageSize={setPageSize} setPageNumber={setPageNumber} filterCharacters={filterCharacters} filterGender={filterGender} filterStatus={filterStatus} search={search} countPages={countPages} pageNumber={pageNumber} showPagination={showPagination} open={open} handleOpen={handleOpen} popupContent={popupContent} handleClose={handleClose}/>
     </div>
   );
 };
